99111b60f6505c3f3c8c83f411d4b7d615d29e86
Maxuel Reis <max@semasoftware.com>
2025-11-11 12:16:19 -0300

    remove coment

--- python/app.py
+++ python/app.py
@@ -1,9 +1,5 @@
 """Simple test script for version check and scan hooks."""
 
-# import jmespath
-
-# print("jmespath version:", jmespath.__version__)
-
 def get_complexity_result():
     """Placeholder for external complexity tools like lizard."""
     pass
